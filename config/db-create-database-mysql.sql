42883d02b4737dd2f629ba98eca0dad3a61bde5e
Allen <mrallen5511@sina.com>
2023-04-10 14:50:01 +0800

    增加数据库创建脚本

---
+++ config/db-create-database-mysql.sql
@@ -0,0 +1,11 @@
+DROP DATABASE if EXISTS mango_admin;
+CREATE DATABASE mango_admin DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+DROP DATABASE if EXISTS mango_user_center;
+CREATE DATABASE mango_user_center DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+DROP DATABASE if EXISTS mango_property;
+CREATE DATABASE mango_property DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+DROP DATABASE if EXISTS mango_logic;
+CREATE DATABASE mango_logic DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
